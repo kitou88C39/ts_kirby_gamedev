3b28f7636767c4afc19409e3d6e34bb28e05d046
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-27 20:59:11 +0900

    src/utils.tsを作成する

--- src/utils.ts
+++ src/utils.ts
@@ -3,5 +3,6 @@ import { scale } from './constants';
 
 export async function makeMap(k: KaboomCtx, name: String) {
   const mapData = await (await fetch(`./${name}.json`)).json();
-  const map = k.make([k.sprite(name)]);
+  const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
+  const spawnPoints: { [key: string]: { x: number; y: number } } = {};
 }
