bba63258e4ccc815785a11a426a0a52b984e1874
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-27 21:02:56 +0900

    src/utils.tsを作成する

--- src/utils.ts
+++ src/utils.ts
@@ -5,4 +5,11 @@ export async function makeMap(k: KaboomCtx, name: String) {
   const mapData = await (await fetch(`./${name}.json`)).json();
   const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
   const spawnPoints: { [key: string]: { x: number; y: number } } = {};
+  for (const layer of mapData.layer) {
+    if (layer.name === 'colliders') {
+      for (const colliders of layer.objects) {
+        map.add();
+      }
+    }
+  }
 }
