2461314ed592c7d8f5104ff3eeff6b4b797226b0
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-28 15:25:13 +0900

    src/entities.tsを作成する

--- src/entities.ts
+++ src/entities.ts
@@ -1,3 +1,5 @@
 import { KaboomCtx } from 'kaboom';
 
-export function makePlayer(k: KaboomCtx);
+export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
+  const player = k.make([]);
+}
